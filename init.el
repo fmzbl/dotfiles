83e0910b307de1c268863f6d11de570d9c224ae1
facu <facu@t480s.168.1.16>
2024-02-05 12:31:50 -0300

    Update emacs config

--- init.el
+++ init.el
@@ -5,6 +5,7 @@
     '("melpa" . "https://melpa.org/packages/") 
     '("gnu-devel" . "https://elpa.gnu.org/devel/"))
 
+(setq package-install-upgrade-built-in t)
 (package-initialize)
 
 ;; Bootstrap 'use-package'
@@ -19,33 +20,65 @@
 (setq use-package-always-ensure t)
 
 ;; osx copy and paste
-(defun wgr/paste-from-osx ()
-  (shell-command-to-string "pbpaste"))
-(defun wgr/copy-to-osx (text &optional push)
-  (let ((process-connection-type nil))
-    (let ((proc (start-process "pbcopy" "*Messages*" "pbcopy")))
-      (process-send-string proc text)
-      (process-send-eof proc))))
-(setq interprogram-cut-function 'wgr/copy-to-osx)
-(setq interprogram-paste-function 'wgr/paste-from-osx)
+;(defun wgr/paste-from-osx ()
+;  (shell-command-to-string "pbpaste"))
+;(defun wgr/copy-to-osx (text &optional push)
+;  (let ((process-connection-type nil))
+;    (let ((proc (start-process "pbcopy" "*Messages*" "pbcopy")))
+;      (process-send-string proc text)
+;      (process-send-eof proc))))
+;(setq interprogram-cut-function 'wgr/copy-to-osx)
+;(setq interprogram-paste-function 'wgr/paste-from-osx)
 
 ;; BASIC STUFF
+;; save session
+ (desktop-save-mode 1)
+
+;; command autocomplete
+(use-package vertico
+  :ensure t
+  :config
+  (vertico-mode 1))
+
+;(use-package marginalia
+;  :ensure t
+;  :config
+;  (marginalia-mode 1))
+
+(use-package ivy
+  :ensure t
+  :config
+  (ivy-mode 1))
+
+;; projectile
+(use-package projectile
+  :ensure t
+  :config
+  (projectile-mode 1)
+  (projectile-global-mode)
+  (setq projectile-completion-system 'ivy)
+  (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map))
+
 ;; disable menu stuff
 (setq inhibit-startup-screen t)
 (menu-bar-mode 0)
 (tool-bar-mode 0)
 
 ;; stop from overwritting config file
-(setq disabled-command-function nil)
-(setq custom-file "~/.emacs-custom.el")
-(load custom-file 'noerror)
+(setq backup-directory-alist
+      `(("." . "~/.emacs.d/backups")))
+(setq auto-save-file-name-transforms
+      `((".*" "~/.emacs.d/auto-save-list/" t)))
+
+(setq custom-file "~/.emacs.d/emacs-custom.el")
+(load custom-file t)
 
 ;; line numbers
 (global-display-line-numbers-mode 1)
 (setq display-line-numbers-type 'relative)
 
 ;; font
-(set-face-attribute 'default nil :height 140)
+(set-face-attribute 'default nil :height 110)
 
 ;; theme
 (use-package solarized-theme
@@ -60,7 +93,6 @@
   :config
   (global-git-gutter-mode 1))
 
-
 ;; VIM AND EVIL MODE
 ;; Vim style undo not needed for emacs 28
 (use-package undo-fu)
@@ -74,19 +106,106 @@
   :config
   (evil-mode 1))
 
-
 ;; LANGUAGES AND LSP
-;(use-package lsp-mode
-;  :commands lsp
-;  :init
-;  ;(setq lsp-keymap-prefix "C-c l")  ;; Or 'C-l', 's-l'
-;  :config
-;  (require 'lsp-clients))
+(use-package rustic
+  :ensure
+  :bind (:map rustic-mode-map
+              ;; ("M-j" . lsp-ui-imenu)
+              ("M-?" . lsp-find-references)
+              ("C-c C-c l" . flycheck-list-errors)
+              ("C-c C-c a" . lsp-execute-code-action)
+              ("C-c C-c r" . lsp-rename)
+              ("C-c C-c q" . lsp-workspace-restart)
+              ("C-c C-c Q" . lsp-workspace-shutdown)
+              ("C-c C-c s" . lsp-rust-analyzer-status))
+  :config
+  ;; uncomment for less flashiness
+  ;; (setq lsp-eldoc-hook nil)
+  ;; (setq lsp-enable-symbol-highlighting nil)
+  ;; (setq lsp-signature-auto-activate nil)
 
-;(use-package lsp-ui)
+  (setq rustic-format-on-save t)
+  (add-hook 'rustic-mode-hook 'rk/rustic-mode-hook))
 
-(use-package which-key
-    :config
-    (which-key-mode))
+(defun rk/rustic-mode-hook ()
+  ;; so that run C-c C-c C-r works without having to confirm, but don't try to
+  ;; save rust buffers that are not file visiting. Once
+  ;; https://github.com/brotzeit/rustic/issues/253 has been resolved this should
+  ;; no longer be necessary.
+  (when buffer-file-name
+    (setq-local buffer-save-without-query t))
+  (add-hook 'before-save-hook 'lsp-format-buffer nil t))
 
-(use-package rust-mode)
+(use-package lsp-mode
+  :ensure
+  :commands lsp
+  :custom
+  ;; what to use when checking on-save. "check" is default, I prefer clippy
+  (lsp-rust-analyzer-cargo-watch-command "clippy")
+  (lsp-eldoc-render-all t)
+  (lsp-idle-delay 0.5)
+  ;; enable / disable the hints as you prefer:
+  (lsp-inlay-hint-enable nil)
+  ;; These are optional configurations. See https://emacs-lsp.github.io/lsp-mode/page/lsp-rust-analyzer/#lsp-rust-analyzer-display-chaining-hints for a full list
+  (lsp-rust-analyzer-display-lifetime-elision-hints-enable "skip_trivial")
+  (lsp-rust-analyzer-display-chaining-hints t)
+  (lsp-rust-analyzer-display-lifetime-elision-hints-use-parameter-names nil)
+  (lsp-rust-analyzer-display-closure-return-type-hints t)
+  (lsp-rust-analyzer-display-parameter-hints nil)
+  (lsp-rust-analyzer-display-reborrow-hints nil))
+  ;; :config
+  ;; (add-hook 'lsp-mode-hook 'lsp-ui-mode))
+
+;; (use-package lsp-ui
+;;   :ensure
+;;   :commands lsp-ui-mode
+;;   :custom
+;;   (lsp-ui-peek-always-show t)
+;;   (lsp-ui-sideline-show-hover t)
+;;   (lsp-ui-doc-enable nil))
+
+;; completion
+(use-package company
+  :ensure
+  :custom
+  (company-idle-delay nil) ;; how long to wait until popup
+  :bind
+  (:map company-mode-map
+    ("<tab>". tab-indent-or-complete)
+    ("TAB". tab-indent-or-complete)))
+
+(use-package yasnippet
+  :ensure
+  :config
+  (yas-reload-all)
+  (add-hook 'prog-mode-hook 'yas-minor-mode)
+  (add-hook 'text-mode-hook 'yas-minor-mode))
+
+
+(defun company-yasnippet-or-completion ()
+  (interactive)
+  (or (do-yas-expand)
+      (company-complete-common)))
+
+(defun check-expansion ()
+  (save-excursion
+    (if (looking-at "\\_>") t
+      (backward-char 1)
+      (if (looking-at "\\.") t
+        (backward-char 1)
+        (if (looking-at "::") t nil)))))
+
+(defun do-yas-expand ()
+  (let ((yas/fallback-behavior 'return-nil))
+    (yas/expand)))
+
+(defun tab-indent-or-complete ()
+  (interactive)
+  (if (minibufferp)
+      (minibuffer-complete)
+    (if (or (not yas/minor-mode)
+            (null (do-yas-expand)))
+        (if (check-expansion)
+            (company-complete-common)
+          (indent-for-tab-command)))))
+
