dff14fe634ef2b553117cce27488ef76efd21c7a
Facundo Mendizabal <fmendizabalpiriz@gmail.com>
2024-04-19 19:08:01 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,3 +2,5 @@
 
 # Audio Interface (UMC202HD)
 echo "options snd_usb_audio quirk_flags=0x20010" | sudo tee -a /etc/modprobe.d/snd_usb_audio.conf
+# Restart pipewire when it doesnt recognize the device
+systemctl --user restart pipewire
